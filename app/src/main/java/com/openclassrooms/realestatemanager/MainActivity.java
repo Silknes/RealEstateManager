3bfd35e4865976315ab457da837f6e16c798cbed
Silknes <eliott.dupau@gmail.com>
2019-03-25 08:27:37 +0100

    Add Toolbar and Navigation Drawer + Add method that convert euro in dollars + Modify method that return the date

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,8 +1,18 @@
 package com.openclassrooms.realestatemanager;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*
  First bug : textViewMain was plug with a TextView from the second activity layout
@@ -12,7 +22,9 @@ import android.widget.TextView;
  Resolve it by change the type of quantity to String and add : "" + before Utils.convert...
 */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     private TextView textViewMain;
     private TextView textViewQuantity;
@@ -22,12 +34,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.configureToolbar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
+
         this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
         this.configureTextViewQuantity();
     }
+
+    private void configureToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void configureDrawerLayout(){
+        drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureNavigationView(){
+        NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_toolbar_item_add_property :
+                Toast.makeText(this, R.string.toolbar_item_add, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_toolbar_item_edit_property :
+                Toast.makeText(this, R.string.toolbar_item_edit, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_toolbar_item_search_property :
+                Toast.makeText(this, R.string.toolbar_item_search, Toast.LENGTH_SHORT).show();
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.activity_main_drawer_map :
+                Toast.makeText(this, R.string.drawer_item_map, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.activity_main_drawer_logout :
+                Toast.makeText(this, R.string.drawer_item_logout, Toast.LENGTH_SHORT).show();
+                break;
+            default :
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(this.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else super.onBackPressed();
+    }
+
+    /***************************/
 
     private void configureTextViewMain(){
         this.textViewMain.setTextSize(15);
